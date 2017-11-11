3cd263990bb78fcbde4f4fa29d931b5b67ceaad8
David Thompson <drthusker@gmail.com>
2017-11-11 09:39:20 -0700

    updated link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-    function validate(form)
-    {
+
   //variables
       var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
       var counter = 10;
@@ -57,7 +56,6 @@
 
 
 
-      }
 
 
 }
